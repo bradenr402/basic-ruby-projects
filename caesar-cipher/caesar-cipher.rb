deb5a7069c18024292a37a7ddeb08810cfc42152
Braden <108199884+bradenr402@users.noreply.github.com>
2023-07-27 13:06:26 -0400

    caesar-cipher project added from computer

---
+++ caesar-cipher/caesar-cipher.rb
@@ -0,0 +1,23 @@
+def caesar_cipher(string, shift)
+  new_string = string.split('').map do |char|
+    if char.match(/[a-z]/)
+      char = char.ord + shift
+      char = char - 26 until char <= 122
+      char = char + 26 until char >= 97
+      char.chr
+    elsif char.match(/[A-Z]/)
+      char = char.ord + shift
+      char = char - 26 until char <= 90
+      char = char + 26 until char >= 65
+      char.chr
+    else char
+    end
+  end
+  return new_string.join('')
+end
+
+puts "What would you like to apply the Caesar cipher to?"
+input_string = gets.chomp
+puts "How many places would you like to shift by?"
+input_shift = gets.chomp.to_i
+puts caesar_cipher(input_string, input_shift)
